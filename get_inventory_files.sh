434426266a76ebf055122bc4a7a4035dc35d7b28
Mohammed Diaa <md@mhmdiaa.com>
2023-03-03 14:11:56 +0200

    Create get_inventory_files.sh

---
+++ get_inventory_files.sh
@@ -0,0 +1,21 @@
+# Example usage
+#
+# get_inventory_files Company file_type
+# get_inventory_files Apple hostnames
+# get_inventory_files SnapChat urls | tee snap_urls.txt
+#
+#
+# File types: hostnames, servers, spider, urls, and cloud
+
+get_inventory_files() {
+    company=$1
+    file=$2
+
+    BASE_URL="https://raw.githubusercontent.com/trickest/inventory/main"
+
+    wget -q -O - "${BASE_URL}/${company}/${file}.txt"
+    i=0
+    while wget -q -O - "${BASE_URL}/${company}/${file}_$(printf '%02d' $i).txt"; do
+        i=$((i+1))
+    done
+}
